93afcdd50375fc48cc3d456ad62b9c5d5056dc99
hjw934131 <hjw934131@users.noreply.github.com>
2015-03-02 10:57:40 +0800

    Create hw1.pas
    20150302作业

---
+++ hw1.pas
@@ -0,0 +1,16 @@
+type
+ pos=record
+   x:integer;
+   y:integer;
+end;
+var
+p1,p2:pos;
+mx,my:integer;
+begin
+readln(p1.x,p1.y);
+readln(p2.x,p2.y);
+mx:=p1.x-p2.x;
+my:=p1.y-p2.y;
+writeln(sqrt(sqr(mx)+sqr(my)):0:2);
+readln;
+end.
