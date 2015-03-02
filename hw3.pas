27cf9c4a93062a017cdbb838980e7507ad0a749a
hjw934131 <hjw934131@users.noreply.github.com>
2015-03-02 11:05:45 +0800

    Create hw3.pas
    20150302作业：3

---
+++ hw3.pas
@@ -0,0 +1,25 @@
+type
+  info=record
+    no:integer;
+    yw:real;
+    sx:real;
+    yy:real;
+    zf:real;
+end;
+var
+student:array [1..100] of info;
+top5:array [1..5] of info;
+ni:integer;
+acn:integer;
+begin
+for ni:=1 to 10 do
+begin
+acn:=n;
+with student[ni] do
+begin
+readln(no,yw,sx,yy);
+end;
+end;
+
+
+end.
